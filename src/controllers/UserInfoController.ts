eaa356e9fd991c1038bb016f4f065f7e973e1dd7
lambrosopos <lambrosopos@gmail.com>
2020-04-27 19:25:27 +0900

    :sparkles: User functions
     - Userinfo to get users
     - User post to signup ready

--- src/controllers/UserInfoController.ts
+++ src/controllers/UserInfoController.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
-import { OK } from "http-status-codes";
+import { OK, BAD_REQUEST } from "http-status-codes";
+import User from "../models/Users";
 
 export default {
-  get: (req: Request, res: Response) => {
-    return res.status(OK).end();
+  get: async (req: Request, res: Response) => {
+    User.find((error: Error, users: any) => {
+      if (error) return res.sendStatus(BAD_REQUEST);
+      return res.status(OK).json(users);
+    });
   },
 };
