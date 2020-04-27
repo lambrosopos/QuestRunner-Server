b6656d96d267bef025cda8b2b7227c5d454d958c
lambrosopos <lambrosopos@gmail.com>
2020-04-27 15:58:37 +0900

    :alien: API Change from (quest -> quests) in get

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const express_1 = require("express");
-const Users_1 = tslib_1.__importDefault(require("./Users"));
 const router = express_1.Router();
-router.use('/users', Users_1.default);
+router.use("/user", require("./UserRouter"));
+router.use("/userinfo", require("./UserInfoRouter"));
+router.use("/avatar", require("./AvatarRouter"));
+router.use("/quest", require("./QuestRouter"));
+router.use("/rank", require("./RankRouter"));
+router.use("/items", require("./ItemsRouter"));
 exports.default = router;
