271311f5bd63f100d3898b6033e75d6659193705
lambrosopos <lambrosopos@gmail.com>
2020-04-29 17:50:26 +0900

    :card_file_box: Changing model structure for mongodb

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,51 +1,85 @@
 import mongoose, { Schema, mongo } from "mongoose";
+import bcrypt from "bcrypt";
 
 export type UserDocument = mongoose.Document & {
-  user: {
-    username: string;
-    password: string;
-    email: string;
-    profilePic: string;
-    motto: string;
-    active: {
-      experiencebar: number;
-      background: number;
-      darkmode: boolean;
-    };
-    experience: number;
-    credits: number;
-    quests: [];
-    created_at: Date;
+  username: string;
+  password: string;
+  email: string;
+  profilePic: string;
+  motto: string;
+  active: {
+    experiencebar: number;
+    background: number;
+    darkmode: boolean;
   };
+  experience: number;
+  credits: number;
+  quests: [];
+  created_at: Date;
   todolist: [];
   items: object;
 };
 
 const UserSchema = new Schema(
   {
-    user: {
-      username: String,
-      password: String,
-      email: String,
-      profilePic: String,
-      motto: String,
-      active: {
-        experiencebar: Number,
-        background: Number,
-        darkmode: Boolean,
-      },
-      experience: Number,
-      credits: Number,
-      quests: Array,
-      created_at: { type: Date, default: Date.now },
+    username: String,
+    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      dropDups: true,
+      unique: true,
     },
+    profilePic: String,
+    motto: String,
+    active: {
+      experiencebar: Number,
+      background: Number,
+      darkmode: Boolean,
+    },
+    experience: Number,
+    credits: Number,
+    quests: Array,
+
     todolist: Array,
     items: {
       experiencebar: Array,
       background: Array,
     },
+    created_at: { type: Date, default: Date.now },
   },
   { collection: "Users" }
 );
 
-export const User = mongoose.model("User", UserSchema);
+UserSchema.pre("save", function (next) {
+  var user = this as UserDocument;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (
+  candidatePassword: any,
+  cb: any
+) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+export const User = mongoose.model<UserDocument>("User", UserSchema);
