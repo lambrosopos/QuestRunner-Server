9f99c62e25c3553287893208b7658e068d22c855
lambrosopos <lambrosopos@gmail.com>
2020-05-01 12:35:43 +0900

    :card_file_box: Creating item data schema

---
+++ src/models/Store.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+export type ItemDocument = mongoose.Document & {
+  feature: string;
+  category: string;
+  price: number;
+  image: string;
+};
+
+const ItemSchema = new Schema(
+  {
+    name: String,
+    category: String,
+    price: Number,
+    image: String,
+    created_at: { type: Date, default: Date.now },
+  },
+  { collection: "Items" }
+);
+
+export const Item = mongoose.model<ItemDocument>("Item", ItemSchema);
