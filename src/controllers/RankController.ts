bfb3874ca2c71ada84d80d51ebdd600144d8104a
lambrosopos <lambrosopos@gmail.com>
2020-05-04 14:10:30 +0900

    :zap: Filtering results to include necessary fields

--- src/controllers/RankController.ts
+++ src/controllers/RankController.ts
@@ -4,12 +4,15 @@ import { User } from '@models/Users';
 
 export default {
   get: async (req: Request, res: Response) => {
-    const rankLimit: any = req.query.top;
+    const rankLimit = Number(req.query.top);
 
-    const queryResults = await User.find()
+    const queryResults = await User.find(
+      {},
+      { username: 1, profilePic: 1, motto: 1, experience: 1 }
+    )
       .sort({ experience: -1 })
       .limit(rankLimit);
 
-    return res.status(OK).end(queryResults);
+    return res.status(OK).json(queryResults);
   },
 };
