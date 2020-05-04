011a19c19d16efbbc21367f562828bd98006e41d
lambrosopos <lambrosopos@gmail.com>
2020-05-04 15:47:59 +0900

    :zap: Changing message contents in response

--- src/middlewares/jwtAuthenticator.ts
+++ src/middlewares/jwtAuthenticator.ts
@@ -1,12 +1,12 @@
-import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
-import { UNAUTHORIZED, BAD_REQUEST } from "http-status-codes";
-require("dotenv").config();
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { UNAUTHORIZED, BAD_REQUEST } from 'http-status-codes';
+require('dotenv').config();
 
-export const accessTokenExpiresIn = "1h";
-export const refreshTokenExpiresIn = "1d";
-export const accessTokenSecret = "@questRunner";
-export const refreshTokenSecret = "@runDev";
+export const accessTokenExpiresIn = '1h';
+export const refreshTokenExpiresIn = '1d';
+export const accessTokenSecret = '@questRunner';
+export const refreshTokenSecret = '@runDev';
 export const refreshTokenList = new Array();
 
 export const authenticateJWT = (
@@ -19,12 +19,12 @@ export const authenticateJWT = (
   if (authHeader) {
     const tokenToVerify = authHeader;
 
-    jwt.verify(tokenToVerify, "@questRunner", (err: any, user: any) => {
+    jwt.verify(tokenToVerify, '@questRunner', (err: any, user: any) => {
       if (err) return res.sendStatus(UNAUTHORIZED);
       req.user = user;
       next();
     });
   } else {
-    return res.sendStatus(BAD_REQUEST);
+    return res.sendStatus(UNAUTHORIZED).json({ message: 'token missing' });
   }
 };
