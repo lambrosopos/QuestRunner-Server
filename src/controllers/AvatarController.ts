06b3b381d0b83a7974333a302962bf94d6fdb81d
lambrosopos <lambrosopos@gmail.com>
2020-05-06 13:33:11 +0900

    :heavy_plus_sign: Adding multer
     - Allowing process handling of multipart form data

--- src/controllers/AvatarController.ts
+++ src/controllers/AvatarController.ts
@@ -1,8 +1,24 @@
-import { Request, Response } from "express";
-import { OK } from "http-status-codes";
+import { Request, Response } from 'express';
+import { OK, BAD_REQUEST, NOT_FOUND } from 'http-status-codes';
+import { User, UserDocument } from '@models/Users';
 
 export default {
   get: (req: Request, res: Response) => {
+    const userID = req.user.uid;
+
+    User.findById(userID, (err: any, doc: UserDocument) => {
+      if (err)
+        return res
+          .status(BAD_REQUEST)
+          .json({ message: 'DB error in finding ID' });
+      if (doc) {
+        return res
+          .status(OK)
+          .json({ messsage: 'Success', url: doc.profilePic });
+      } else {
+        return res.status(NOT_FOUND).json({ message: 'User not found' });
+      }
+    });
     return res.status(OK).end();
   },
   put: (req: Request, res: Response) => {
