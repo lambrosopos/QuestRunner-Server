694116c65a08f9a194ded5e4435eee6295793263
lambrosopos <lambrosopos@gmail.com>
2020-05-07 16:55:10 +0900

    :flag: Updating dist folder

--- dist/routes/RankRouter.js
+++ dist/routes/RankRouter.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const _controllers_1 = require("@controllers/");
 const router = express_1.Router();
-router.get('/', _controllers_1.RankController.get);
+router.get('/', _controllers_1.RankController.topRanks);
 module.exports = router;
