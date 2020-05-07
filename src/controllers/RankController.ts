af6fdfc13949f83bec17e5d58b09f0875411f4c1
lambrosopos <lambrosopos@gmail.com>
2020-05-07 16:36:50 +0900

    :sparkles: Added myrank feature

--- src/controllers/RankController.ts
+++ src/controllers/RankController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { OK } from 'http-status-codes';
-import { User } from '@models/Users';
+import { OK, BAD_REQUEST, NOT_FOUND } from 'http-status-codes';
+import { User, UserDocument } from '@models/Users';
 
 export default {
-  get: async (req: Request, res: Response) => {
+  topRanks: async (req: Request, res: Response) => {
     const rankLimit = Number(req.query.top);
 
     const queryResults = await User.find(
@@ -14,5 +14,39 @@ export default {
       .limit(rankLimit);
 
     return res.status(OK).json(queryResults);
+  },
+  myRank: async (req: Request, res: Response) => {
+    const userID = req.user.uid;
+
+    let queryResults: any = await User.find(
+      {},
+      {
+        experience: 1,
+        username: 1,
+      }
+    ).sort({ experience: -1 });
+
+    let refinedResults = await Array.prototype.reduce.call(
+      queryResults,
+      (acc: any, curr: any, currIdx: number) => {
+        if (String(curr._id) === userID) {
+          return {
+            username: curr.username,
+            rank: currIdx,
+          };
+        } else {
+          return acc;
+        }
+      },
+      null
+    );
+
+    if (refinedResults) {
+      return res.status(OK).json(refinedResults);
+    } else {
+      return res
+        .status(NOT_FOUND)
+        .json({ success: false, message: 'User has no rank' });
+    }
   },
 };
