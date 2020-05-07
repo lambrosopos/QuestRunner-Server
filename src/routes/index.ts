52b9911570654f4c13dac4a7a8c4cdfa658e3733
lambrosopos <lambrosopos@gmail.com>
2020-05-07 13:51:33 +0900

    :triangular_flag_on_post: Updating routes
     - Changes due from method name changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 // For simple get routes
-import { QuestController, UserController } from "@controllers/";
-import { authenticateJWT } from "../middlewares/jwtAuthenticator";
+import { QuestController, UserController } from '@controllers/';
+import { authenticateJWT } from '../middlewares/jwtAuthenticator';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use("/user", require("./UserRouter"));
-router.use("/avatar", require("./AvatarRouter"));
-router.use("/quest", require("./QuestRouter"));
-router.use("/rank", require("./RankRouter"));
-router.use("/items", require("./ItemsRouter"));
+router.use('/user', require('./UserRouter'));
+router.use('/avatar', require('./AvatarRouter'));
+router.use('/quest', require('./QuestRouter'));
+router.use('/rank', require('./RankRouter'));
+router.use('/items', require('./ItemsRouter'));
 
 // Adding simple get routes
-router.get("/quests", QuestController.get);
-router.get("/userinfo", authenticateJWT, UserController.get);
-router.post("/userlogin", UserController.postSignin);
+router.get('/quests', QuestController.get);
+router.get('/userinfo', authenticateJWT, UserController.getInfo);
+router.post('/userlogin', UserController.signin);
 
 // Export the base-router
 export default router;
